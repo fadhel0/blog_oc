c8c72c5aa0ff5ff490f5e024fa363ba64e9a26a0
Fadhel <fadhel@protonmail.com>
2018-04-09 12:05:16 +0100

    add some modifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,35 +8,4 @@ import { PostComponent } from './post/post.component';
 })
 export class AppComponent {
 
-  posts = [
-    {
-      title : 'Mon premier post',
-      content : 'Ad qui mollit reprehenderit ad eu id amet dolor sunt sit laborum. ' +
-        'Nostrud amet officia laboris laboris veniam esse dolore ullamco aliquip consequat sit.' +
-        'Est qui veniam excepteur consequat Lorem eu exercitation consequat in laborum laborum sint.' +
-        'Sint ullamco Lorem nulla commodo ullamco laboris proident consectetur quis laborum ex.',
-      loveIts : 1,
-      created_at : new Date()
-    },
-
-    {
-      title: 'Mon deuxieme post',
-      content: 'Ad qui mollit reprehenderit ad eu id amet dolor sunt sit laborum. ' +
-        'Nostrud amet officia laboris laboris veniam esse dolore ullamco aliquip consequat sit.' +
-        'Est qui veniam excepteur consequat Lorem eu exercitation consequat in laborum laborum sint.' +
-        'Sint ullamco Lorem nulla commodo ullamco laboris proident consectetur quis laborum ex.',
-      loveIts: -1,
-      created_at: new Date()
-    },
-
-    {
-      title: 'Un autre post',
-      content: 'Ad qui mollit reprehenderit ad eu id amet dolor sunt sit laborum. ' +
-        'Nostrud amet officia laboris laboris veniam esse dolore ullamco aliquip consequat sit.' +
-        'Est qui veniam excepteur consequat Lorem eu exercitation consequat in laborum laborum sint.' +
-        'Sint ullamco Lorem nulla commodo ullamco laboris proident consectetur quis laborum ex.',
-      loveIts: 0,
-      created_at: new Date()
-    },
-  ];
 }
